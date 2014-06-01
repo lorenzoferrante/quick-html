29cd3f7ca971bcd0f66d17526d5cb8ec240e1aaf
Lorenzo Ferrante <lorenzoferrante@me.com>
2014-06-01 14:59:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ mkdirp('Web-Template/js', function(err) {
 });
 
 fs.writeFile('Web-Template/index.html', 
-	'<!DOCTYPE HTML>\n<html>\n  <head>\n  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\n  <title>Your Website</title>\n  <link rel="stylesheet" type="text/css" href="index.css">\n  <script type="text/javascript" src="app.js"></script>\n  </head>\n<body>\n</body>\n</html>', function (err) {
+	'<!DOCTYPE HTML>\n<html>\n  <head>\n  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\n  <title>Your Website</title>\n  <link rel="stylesheet" type="text/css" href="css/index.css">\n  <script type="text/javascript" src="app.js"></script>\n  </head>\n<body>\n</body>\n</html>', function (err) {
     if (err) throw err;
     console.log('Created HTML file!');
 });
